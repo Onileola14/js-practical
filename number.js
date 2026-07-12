b56d5439d608aebfe3732a67604b71a3ff2d6ab0
onileola <onileola14@gmail.com>
2026-07-12 23:27:56 +0100

    modified:   number.js

--- number.js
+++ number.js
@@ -4,3 +4,22 @@ console.log(23 === 23.0);
 
 console.log(Number.parseInt('30px'));
 console.log(Number.parseInt('e30px'));
+
+console.log(Number.parseInt('2.5rem'));
+
+console.log(Number.parseFloat('2.5rem'));
+console.log(Number.parseFloat('2.5rem').toFixed(2));
+
+
+
+
+//check if value is not a number
+const x = 20
+console.log(Number.isNaN(x));
+Number.isNaN(x)? console.log('x is not a number') : console.log('x is a number');
+
+console.log(Number.isFinite(x));
+
+//
+
+
